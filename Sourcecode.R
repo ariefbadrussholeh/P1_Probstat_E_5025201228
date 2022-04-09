8e4896e8488315f69aef022e3695a5147991cc57
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-04-10 01:08:44 +0700

    feat: add soal 2

--- Sourcecode.R
+++ Sourcecode.R
@@ -1,6 +1,7 @@
 # 1 ------------------------ 
 x = 3
 prob = 0.20
+
 ## a
 dgeom(x, prob)
 
@@ -18,4 +19,21 @@ hist(data1)
 ## e
 mean(data1)
 var(data1)
+
+# 2 ------------------------ 
+x = 4
+size = 20
+prob = 0.2
+
+## a
+dbinom(x, size, prob) ;data2
+
+## b
+n = 100
+data2 = rbinom(n, size, prob)
+hist(data2)
+
+## c
+mean(data2)
+var(data2)
 
